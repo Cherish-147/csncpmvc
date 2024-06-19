e91d3823f334c3a64a533c7da5dd6ebde058d980
陆梓鹏 <754868159@qq.com>
2024-06-19 18:29:48 +0800

    两个form表单，购买功能还未写

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -5,6 +5,8 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using sales.BLL;
+using sales.DAL;
+
 namespace csncpmvc.Controllers
 {
     public class UserController : Controller
@@ -69,27 +71,102 @@ namespace csncpmvc.Controllers
 
 
 
-        // POST: Product/Buy
-        [HttpPost]
-        public ActionResult Buy(string goods_id)
+        
+        public ActionResult ToBuySelectedProduct(int selectedProductId)
         {
-            if (string.IsNullOrEmpty(goods_id))
+            var selectedProduct = productService.GetProductByIds(selectedProductId);
+            if (selectedProduct == null)
             {
-                ModelState.AddModelError("", "请选择一项商品，再点击购买");
-                return RedirectToAction("Index");
+                return HttpNotFound();
+            }
+
+            // 处理选定的产品逻辑，例如重定向到详细信息页面或显示详细信息
+            // 这里假设重定向到详细信息页面
+            return RedirectToAction("Shopping", new { id = selectedProductId });
+            
+        }
+
+       
+        //// GET: Product/Shopping
+        public ActionResult Shopping(int?id)
+        {
+            if (!id.HasValue)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest, "The id parameter is required.");
+            }
+
+            var product = productService.GetProductByIds(id.Value);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(product); // 确保返回的是单个产品对象
+        }
+
+
+
+
+        public string Purchase(int number, string inventory, string price, string goodsname, string Username, string goodsid)
+        {
+            int Goods = int.Parse(goodsid);
+            if (number == 0)
+            {
+                throw new Exception("请输入购买数量");
             }
             else
             {
-                // 执行购买逻辑
-                return RedirectToAction("Shopping"); // 假设重定向到购物页面
+                if (number > int.Parse(inventory))
+                {
+                    throw new Exception("购买数量不能大于库存");
+                }
+                else
+                {
+                    using (var db = new GreenFoodSalesModel1())
+                    {
+                        // 修改产品表中的库存
+                        var product = db.产品表.SingleOrDefault(p => p.goods_id == Goods);
+
+                        if (product != null)
+                        {
+                            product.stock_quantiy -= number; // 更新库存数量
+                            db.SaveChanges(); // 保存更改到数据库
+                        }
+                        else
+                        {
+                            throw new Exception("未找到指定商品");
+                        }
+
+                        // 判断该商品是否存在购物车中。如果存在就修改购买数量，否则增加一条新的购买记录
+                        var cartItem = db.购物车表.SingleOrDefault(r => r.username == Username && r.goods_id == goodsid);
+
+                        if (cartItem != null)
+                        {
+                            cartItem.Qty += number;
+                        }
+                        else
+                        {
+                            var newCartItem = new sales.DAL.购物车表
+                            {
+                                username = Username,
+                                goods_id = goodsid,
+                                Proname = goodsname,
+                                ListPrice = decimal.Parse(price),
+                                Qty = number
+                            };
+
+                            db.购物车表.Add(newCartItem); // 插入新项
+                        }
+
+                        db.SaveChanges(); // 保存更改到数据库
+                    }
+                }
             }
+
+            return goodsid.ToString();
         }
 
-        // GET: Product/Shopping
-        public ActionResult Shopping()
-        {
-            return View();
-        }
+
 
     }
 }
